c22c399837848e436ac09897b22d27fea1e8414d
Blessed-Wang <blessedwmm@gmail.com>
2021-11-05 17:04:46 +0800

    Swagger属性配置

--- zhili-home-project/src/main/java/com/zhouzhili/zhilihomeproject/properties/SwaggerProperties.java
+++ zhili-home-project/src/main/java/com/zhouzhili/zhilihomeproject/properties/SwaggerProperties.java
@@ -14,6 +14,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "swagger.info")
 public class SwaggerProperties {
 
+
     /**
      * 文档的标题
      */
