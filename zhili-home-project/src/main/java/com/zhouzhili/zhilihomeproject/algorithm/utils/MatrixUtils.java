f7f6a6e6972ecef51fc09f82ab143980d6c3ee60
Blessed-Wang <blessedwmm@gmail.com>
2022-02-06 11:32:46 +0800

    矩阵工具类

--- zhili-home-project/src/main/java/com/zhouzhili/zhilihomeproject/algorithm/utils/MatrixUtils.java
+++ zhili-home-project/src/main/java/com/zhouzhili/zhilihomeproject/algorithm/utils/MatrixUtils.java
@@ -1,8 +1,8 @@
 package com.zhouzhili.zhilihomeproject.algorithm.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+//import org.opencv.core.CvType;
+//import org.opencv.core.Mat;
 import org.springframework.util.Assert;
 
 /**
@@ -15,27 +15,27 @@ import org.springframework.util.Assert;
 @Slf4j
 public class MatrixUtils {
 
-    public static String printMatrix(Mat matrix) {
-        Assert.notNull(matrix, "matrix is null!");
-        int m = matrix.rows();
-        int n = matrix.cols();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                double[] value = matrix.get(i, j);
-                for (double d : value) {
-                    sb.append(d);
-                    sb.append("\t");
-                }
-            }
-            sb.append("\b\n");
-        }
-        return sb.toString();
-    }
-
-    public static void main(String[] args) {
-
-        Mat m = Mat.zeros(3, 4, CvType.CV_8U);
-        printMatrix(m);
-    }
+//    public static String printMatrix(Mat matrix) {
+//        Assert.notNull(matrix, "matrix is null!");
+//        int m = matrix.rows();
+//        int n = matrix.cols();
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < m; ++i) {
+//            for (int j = 0; j < n; ++j) {
+//                double[] value = matrix.get(i, j);
+//                for (double d : value) {
+//                    sb.append(d);
+//                    sb.append("\t");
+//                }
+//            }
+//            sb.append("\b\n");
+//        }
+//        return sb.toString();
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        Mat m = Mat.zeros(3, 4, CvType.CV_8U);
+//        printMatrix(m);
+//    }
 }
