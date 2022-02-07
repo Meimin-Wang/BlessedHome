f18a49ab7fbd4c9f8d8e03b95002b7d11cc2dc3a
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-07 22:05:06 +0800

    资源服务器配置类

--- zhili-home-project/src/main/java/com/blessed/home/config/security/ResourceServerConfig.java
+++ zhili-home-project/src/main/java/com/blessed/home/config/security/ResourceServerConfig.java
@@ -1,6 +1,6 @@
-package com.zhouzhili.zhilihomeproject.config.security;
+package com.blessed.home.config.security;
 
-import com.zhouzhili.zhilihomeproject.handler.exception.resource.ResourceAccessDeniedExceptionHandler;
+import com.blessed.home.constants.RoleConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,9 +13,11 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+import static com.blessed.home.constants.ResourceConstants.PERSONAL_INFO_RESOURCE_PATH;
+
 /**
  * @ClassName ResourceServerConfig
- * @Description TODO
+ * @Description 资源服务器配置类
  * @Author blessed
  * @Date 2021/11/8 : 21:27
  * @Email blessedwmm@gmail.com
@@ -37,30 +39,23 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(
-                        "/clients/**", "/roles/**", "/countries/**", "/provinces/**", "/cities/**",
                         "/swagger-ui/**", "/v2/**", "/v3/**", "/swagger/**", "/swagger-resources/**", "/springfox/**",
                         "/druid/**",
                         "/public/**",
                         "/static/**",
                         "/webjars/**",
-                        "/profile/**",
-                        "/users",
-                        "/personalInformations/**"
                 ).permitAll()
 
                 // 监控端点
-                .antMatchers("/actuator/**").hasRole("ADMIN")
+                .antMatchers("/actuator/**").hasRole(RoleConstants.ADMIN_ROLE_NAME)
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .antMatchers(HttpMethod.GET, String.format("/%s/search/findPersonalInformationByUserId", PERSONAL_INFO_RESOURCE_PATH)).permitAll()
 
-                // 用户
-//                .antMatchers(HttpMethod.POST, "/users/**").permitAll() // 用户注册
-//                .antMatchers(HttpMethod.GET, "/users").hasRole("ADMIN") // 获取所有用户，只能管理员获取
-//                .antMatchers(HttpMethod.PUT, "/users/**").hasRole("USER") // 更新用户信息，管理员可以更新，用户可以更新自己的信息
-//                .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN") // 删除用户，只能够管理员进行操作
 
                 // 其他资源
                 .anyRequest().authenticated()
-        .and()
-            .exceptionHandling()
+                .and()
+                .exceptionHandling()
         ;
     }
 
@@ -68,7 +63,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources
                 //资源ID
-                .resourceId("zhili-home-resource-server")
+                .resourceId("blessed-home-resource-server")
                 // 用来解决匿名用户访问无权限资源时的异常
                 .authenticationEntryPoint(authenticationEntryPoint)
                 // 访问资源权限相关异常处理
