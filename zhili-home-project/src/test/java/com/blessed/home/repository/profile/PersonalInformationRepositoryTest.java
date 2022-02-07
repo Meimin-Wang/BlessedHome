6ada5fdd0daa06cccc8720af03c76e55a775edc7
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-07 21:22:46 +0800

    PersonalInformation实体仓库方法测试

---
+++ zhili-home-project/src/test/java/com/blessed/home/repository/profile/PersonalInformationRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.blessed.home.repository.profile;
+
+import com.blessed.home.entity.profile.PersonalInformation;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ContextConfiguration
+@Slf4j
+class PersonalInformationRepositoryTest {
+
+    @Autowired PersonalInformationRepository personalInformationRepository;
+
+    @Transactional
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void testFindPersonalInformationByUserId() {
+        Optional<PersonalInformation> personalInfo = personalInformationRepository.findPersonalInformationByUserId(32L);
+        log.info("{}", personalInfo.get());
+    }
+}
