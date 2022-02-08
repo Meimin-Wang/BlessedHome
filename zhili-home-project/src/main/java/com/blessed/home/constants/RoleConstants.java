70f870273831462131a222554ec221ea15b49464
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-08 20:23:18 +0800

    角色实体相关常量

---
+++ zhili-home-project/src/main/java/com/blessed/home/constants/RoleConstants.java
@@ -0,0 +1,12 @@
+package com.blessed.home.constants;
+
+/**
+ * 四种角色名称
+ * @author Blessed
+ */
+public class RoleConstants {
+    public static final String ADMIN_ROLE_NAME = "ADMIN";
+    public static final String USER_ROLE_NAME = "USER";
+    public static final String SUPERVISOR_ROLE_NAME = "SUPERVISOR";
+    public static final String MEMBER_ROLE_NAME = "MEMBER";
+}
