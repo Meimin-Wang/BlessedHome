2a4fc12202af448ba7ba9bb07f06018b2b579de2
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-09 19:45:11 +0800

    博客附件实体，比如图片、视频等

--- zhili-home-project/src/main/java/com/blessed/home/entity/blog/AttachFile.java
+++ zhili-home-project/src/main/java/com/blessed/home/entity/blog/AttachFile.java
@@ -1,16 +1,19 @@
-package com.zhouzhili.zhilihomeproject.entity.blog;
+package com.blessed.home.entity.blog;
 
-import com.zhouzhili.zhilihomeproject.entity.BaseEntity;
-import com.zhouzhili.zhilihomeproject.validator.annotation.MimeType;
+import com.blessed.home.entity.BaseEntity;
+import com.blessed.home.validator.annotation.MimeType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.rest.core.annotation.Description;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * @ClassName AttachFile
@@ -19,23 +22,43 @@ import java.io.Serializable;
  * @Date 2021/11/23 : 12:41
  * @Email blessedwmm@gmail.com
  */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity(name = "tbl_attach_file")
 @Table(value = "tbl_attach_file")
 @ApiModel("博客附件实体")
 public class AttachFile extends BaseEntity implements Serializable {
 
+    /**
+     * 附件类型
+     */
     @Column(name = "mime_type", length = 50)
     @ApiModelProperty("文件类型")
     @Description("文件类型")
     @MimeType(message = "文件类型没有遵循mime type规范")
     private String mimeType;
 
+    /**
+     * 附件地址
+     */
     @URL(message = "附件路径不合法，附件路径必须是URL路径")
     @Column(name = "file_url")
     @ApiModelProperty("附件路径")
     @Description("附件路径")
     private String fileUrl;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AttachFile that = (AttachFile) o;
+        return id != null && Objects.equals(id, that.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
