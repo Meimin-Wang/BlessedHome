a34e864c4511b6bb047183b7eda8fa9d1531fb03
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-09 19:52:19 +0800

    博客评论实体类

--- zhili-home-project/src/main/java/com/blessed/home/entity/blog/Comment.java
+++ zhili-home-project/src/main/java/com/blessed/home/entity/blog/Comment.java
@@ -1,24 +1,18 @@
-package com.zhouzhili.zhilihomeproject.entity.blog;
+package com.blessed.home.entity.blog;
 
-import com.zhouzhili.zhilihomeproject.entity.BaseEntity;
-import com.zhouzhili.zhilihomeproject.entity.security.User;
+import com.blessed.home.entity.BaseEntity;
+import com.blessed.home.entity.security.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.rest.core.annotation.Description;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * @ClassName Comment
@@ -27,26 +21,51 @@ import java.io.Serializable;
  * @Date 2021/11/23 : 13:13
  * @Email blessedwmm@gmail.com
  */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity(name = "tbl_comment")
 @Table(value = "tbl_comment")
 @ApiModel("博客评论实体类")
 @Description("博客实体类")
 public class Comment extends BaseEntity implements Serializable {
 
-    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    /**
+     * 评论者
+     */
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+    /**
+     * 评论内容
+     */
     @Lob
     @ApiModelProperty("评论内容")
     @Description("评论内容")
     @Column(name = "comment_content")
     private String commentContent;
 
+    /**
+     * 评论点赞数
+     */
     @Column(name = "like_count")
     @ApiModelProperty("评论点赞数量")
     @Description("评论点赞数量")
     @Min(value = 0, message = "点赞数量必须大于或等于0")
     private Long likeCount = 0L;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Comment comment = (Comment) o;
+        return id != null && Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
