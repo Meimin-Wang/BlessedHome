47f91817c6fa54236e57a154db81fce4e8387010
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-12 18:32:09 +0800

    论文写作项目

--- zhili-home-project/src/main/java/com/blessed/home/entity/paper/PaperWritingProject.java
+++ zhili-home-project/src/main/java/com/blessed/home/entity/paper/PaperWritingProject.java
@@ -1,10 +1,11 @@
 package com.blessed.home.entity.paper;
 
-import com.blessed.home.entity.security.User;
 import com.blessed.home.entity.BaseEntity;
+import com.blessed.home.entity.security.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.rest.core.annotation.Description;
 
@@ -12,6 +13,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -21,7 +23,10 @@ import java.util.Set;
  * @Date 2021/11/23 : 13:27
  * @Email blessedwmm@gmail.com
  */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity(name = "tbl_pwp")
 @Table(value = "tbl_pwp")
 @ApiModel("论文写作项目")
@@ -37,12 +42,14 @@ public class PaperWritingProject extends BaseEntity implements Serializable {
     @JoinTable(name = "tbl_pwp_supervisors", joinColumns = @JoinColumn(name = "pwp_id"), inverseJoinColumns = @JoinColumn(name = "supervisor_id"))
     @ApiModelProperty("导师")
     @Description("导师")
+    @ToString.Exclude
     private Set<User> supervisors = new HashSet<>();
 
     @ManyToMany(targetEntity = User.class, fetch = FetchType.LAZY)
     @JoinTable(name = "tbl_pwp_authors", joinColumns = @JoinColumn(name = "pwp_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     @ApiModelProperty("论文作者")
     @Description("论文作者")
+    @ToString.Exclude
     private Set<User> authors = new HashSet<>();
 
     @Column(name = "journal_name", length = 100)
@@ -54,4 +61,17 @@ public class PaperWritingProject extends BaseEntity implements Serializable {
     @ApiModelProperty("截止时间")
     @Description("截止时间")
     private Date deadline;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        PaperWritingProject that = (PaperWritingProject) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
