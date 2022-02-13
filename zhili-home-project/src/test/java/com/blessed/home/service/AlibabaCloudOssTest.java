7e8ddbb4f4a20ec122cc9bda8b7f21fc6245f612
Meimin Wang <93699224+Meimin-Wang@users.noreply.github.com>
2022-02-13 18:13:05 +0800

    阿里云OSS服务测试类

--- zhili-home-project/src/test/java/com/blessed/home/service/AlibabaCloudOssTest.java
+++ zhili-home-project/src/test/java/com/blessed/home/service/AlibabaCloudOssTest.java
@@ -5,9 +5,7 @@ import com.blessed.home.config.alibaba.AlibabaCloudOssCredentials;
 import com.blessed.home.properties.AlibabaCloudOssProperties;
 import com.blessed.home.service.impl.AlibabaCloudOssServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,7 +14,7 @@ import java.net.URL;
 
 /**
  * @ClassName AlibabaCloudOssTest
- * @Description TODO
+ * @Description 阿里云OSS服务测试类
  * @Author blessed
  * @Date 2021/11/6 : 20:02
  * @Email blessedwmm@gmail.com
@@ -24,6 +22,7 @@ import java.net.URL;
 @SpringBootTest
 @DisplayName("阿里云OSS存储测试类")
 @Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AlibabaCloudOssTest {
 
     @Autowired
@@ -39,7 +38,8 @@ public class AlibabaCloudOssTest {
     private AlibabaCloudOssServiceImpl alibabaCloudOssService;
 
     @Test
-    @DisplayName("测试OSSClient对象是否能够注入")
+    @Order(1)
+    @DisplayName("1. 测试OSSClient对象获取")
     public void testOssClient() {
         Assertions.assertNotNull(ossClient, "阿里云OSSClient对象为null");
         log.info("ossClient: {}", ossClient.toString());
@@ -54,17 +54,5 @@ public class AlibabaCloudOssTest {
         log.info("Oss secret key: {}", alibabaCloudOssCredentials.getSecretAccessKey());
     }
 
-    @Test
-    @DisplayName("测试OSS文件上传")
-    public void testUploadFile() {
-        URL uploadedFileUrl = alibabaCloudOssService.uploadFile(new File("/Users/blessed/Downloads/lenna.png"), "aaa");
-        log.info("{}", uploadedFileUrl);
-    }
 
-    @Test
-    @DisplayName("测试OSS文件删除")
-    public void testDeleteFile() {
-        String key = "blog/98c-409c-ad23-4d4aee5aff0alenna.png";
-        alibabaCloudOssService.deleteFile(key);
-    }
 }
